5cc5be06d9556e38b85c873ca400bc872b0c0ecc
Mona Lisa <holmesmainsend@gmail.com>
2022-10-23 13:25:11 -0400

    added while loop for game

--- guessinggame.c
+++ guessinggame.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-    
+    int secretNumber = 9, guess;
+
+    while(guess != secretNumber) {
+        printf("Enter a numeric guess: \n");
+        scanf("%d", &guess);
+    }
+
+    printf("Congratulations, you win!\n");
 
     return 0;
 }
