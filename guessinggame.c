3563f1a378efde5e3020fd6baf02b9fa4a678976
Mona Lisa <holmesmainsend@gmail.com>
2022-10-23 13:33:12 -0400

    began adding guess limits

--- guessinggame.c
+++ guessinggame.c
@@ -2,14 +2,22 @@
 #include <stdlib.h>
 
 int main() {
-    int secretNumber = 9, guess;
+    int secretNumber = 9;
+    int guess;
+    int guessCounter = 0;
+    int guessLimit = 3;
+    int outOfGuesses = 0;
 
-    while(guess != secretNumber) {
-        printf("Enter a numeric guess: \n");
-        scanf("%d", &guess);
+    while(guess != secretNumber && outOfGuesses != 1) {
+        if (guessCounter < guessLimit) {
+            printf("Enter a numeric guess: \n");
+            scanf("%d", &guess);
+            guessCounter++;
+        } else
+            outOfGuesses = 1;
     }
 
-    printf("Congratulations, you win!\n");
+    printf("You won in %d guesses!\n", guessCounter);
 
     return 0;
 }
